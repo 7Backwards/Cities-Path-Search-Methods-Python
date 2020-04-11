3be361e0d6de26d958c2a30e543903c11061ec94
Pedro Baptista <pedro.miguel.baptista@gmail.com>
2020-04-11 20:06:24 +0100

    recursion-1

--- SearchGraphIDDFS.py
+++ SearchGraphIDDFS.py
@@ -4,56 +4,53 @@ from SearchGraph import SearchGraph
 
 class SearchGraphIDDFS(SearchGraph):
 
-	def __init__(self, nameMethod, origin, destiny, countryMap, isLimited=True):
-		super().__init__(nameMethod, origin, destiny, countryMap)
-		self.isLimited = isLimited
+    def __init__(self, nameMethod, origin, destiny, countryMap, isLimited=True):
+        super().__init__(nameMethod, origin, destiny, countryMap)
+        self.isLimited = isLimited
 
-		self.limit = 20 #max iteration
-		
-		self.selectedPath = []
+        self.limit = 20
+        self.selectedPath = []
+        self.graph = {}
 
-		self.graph = {}
+        for node in self.countryMap.nodesMap:
+            self.graph[node.name] = []
+            for path in self.countryMap.pathsMap:
+                if (path.city1 == node.name or path.city2 == node.name):
+                    self.graph[node.name].append(path)
 
-		for node in self.countryMap.nodesMap:
-			self.graph[node.name] = []
-			for path in self.countryMap.pathsMap:
-				if (path.city1 == node.name or path.city2 == node.name):
-					self.graph[node.name].append(path)
-					
-		if isLimited:
-			self.searchIDDFSLimited(self.graph[origin], origin, destiny, self.limit)
-		else:
-			self.searchIDDFSLimited(self.graph[origin], origin, destiny, self.limit)
+        if isLimited:
+            self.searchIDDFSLimited(
+                self.graph[origin], origin, destiny, self.limit)
+        else:
+            self.searchIDDFSNotLimited(
+                self.graph[origin], origin, destiny)
 
-				
-				
-	def searchIDDFSLimited(self, node, origin, destiny, limit):
-		if origin == destiny:
-			print("Path equals to destiny")
-			return
-		
-		if len(node) > 0:
-			for op in node:
-				if destiny == op.city1 or destiny == op.city2:
-					print("yyyyeeeeeeaaaahhhhh")
-					return
-		
-		for item in self.graph:
-			self.searchIDDFSLimited(self.graph[item], origin, destiny, limit)
-			
-			
-		'''
-		for place in self.graph:
-			print(place)
-			for item in self.graph[place]:
-				print(item.city1 + " |  " + item.city2)
-		'''
+    def searchIDDFSLimited(self, node, origin, destiny, limit):
+        if origin == destiny:
+            print("Path equals to destiny")
+            return True
+        elif limit == 0:
+            return False
 
-		self.selectedPath.append(self.countryMap.pathsMap[0])
-		self.selectedPath.append(self.countryMap.pathsMap[6])
-		self.selectedPath.append(self.countryMap.pathsMap[8])
+        for op in node:
+            if destiny == op.city1 or destiny == op.city2:
+                print("yyyyeeeeeeaaaahhhhh")
+                return True
 
-		self.iterationList.append("teste 1")
-		self.iterationList.append("teste 2")
-		self.iterationList.append("teste 3")
-		self.iterationList.append("teste 4")
+        for item in self.graph:
+            return self.searchIDDFSLimited(self.graph[item], origin, destiny, limit-1)
+
+    def searchIDDFSNotLimited(self, node, origin, destiny):
+        if origin == destiny:
+            print("Path equals to destiny")
+            return True
+
+        for op in node:
+            if destiny == op.city1 or destiny == op.city2:
+                print("yyyyeeeeeeaaaahhhhh")
+                return True
+
+        for item in self.graph:
+            if self.searchIDDFSLimited(self.graph[item], origin, destiny) == True:
+                return True
+            return False
