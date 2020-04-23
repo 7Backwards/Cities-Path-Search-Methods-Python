d979aed81b03dc6cb8bc7ec49076fe77a9954347
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-04-24 00:27:38 +0100

    ASTAR implemented successfully

--- SearchGraphASTAR.py
+++ SearchGraphASTAR.py
@@ -1,4 +1,5 @@
-# AStar Search
+# UniformCostSearch
+import sys
 from SearchGraph import SearchGraph
 
 
@@ -6,91 +7,128 @@ class SearchGraphASTAR(SearchGraph):
 
     def __init__(self, nameMethod, origin, destiny, countryMap, isLimited, directDistanceHeuristic):
         super().__init__(nameMethod, origin, destiny, countryMap)
-        self.isLimited = False
-
         self.directDistancePaths = directDistanceHeuristic
-        self.selectedPath = []
-        self.graph = {}
-        self.route = []
-        self.minDirectDistanceToDestiny = {}
-        self.minDirectDistanceToDestiny[origin] = "0"
+        self.isLimited = isLimited
+        self.pathList = []
+        self.iterationList = []
+        self.astar(origin, destiny,self.isLimited)
+        if len(self.pathList) > 0:
+            print(self.pathList)
+            truePath = [destiny]
+            lookForNode = destiny
+            while ( lookForNode != origin):
+                for path in self.pathList:
+                    if lookForNode == destiny and path[1] == lookForNode:
+                        
+                        totalcost = path[0]
+                        cost = totalcost - self.get_cost(path[1],path[2])
+                        nodeBefore = path[2]
+                        lookForNode = nodeBefore
+                    elif path[1] == lookForNode and path[0] == cost:
+                        
+                        cost = cost - self.get_cost(path[1],path[2])
+                        node = path[1]
+                        nodeBefore = path[2]
+                        lookForNode = nodeBefore
+                        break
+                truePath.append(lookForNode)   
+            truePath.reverse()
+            i = 0
+            for i in range(0, len(truePath) - 1):
+                for pathMap in self.countryMap.getEdges():
+                    if (truePath[i] == pathMap.city1 and truePath[i+1] == pathMap.city2) or (truePath[i] == pathMap.city2 and truePath[i+1] == pathMap.city1):
+                        self.selectedPath.append(pathMap)
+                i+=1 
+            self.formatIterationList()
 
-        for node in self.countryMap.nodesMap:
-            self.graph[node.name] = []
-            for path in self.countryMap.pathsMap:
-                if path.city1 == node.name or path.city2 == node.name:
-                    self.graph[node.name].append(path)
+        
+        
+    def neighbors(self, node):
+        
+        edges = []
+        for edge in self.countryMap.getEdges():
+            
+            if node == edge.city1:
+                
+                edges.append(edge.city2)
+            elif node == edge.city2:
+                
+                edges.append(edge.city1)
+                
+        return edges
 
-        self.searchAstar(self.graph[origin], origin,
-                         destiny, origin, self.route)
-        self.route.append(origin)
-        self.route.reverse()
-        # Alternative -> self.route(::-1)
-        # print(self.route)
+    def get_cost(self, from_node, to_node):
+        
+        for edge in self.countryMap.getEdges():
+            
+            if (from_node == edge.city1 and to_node == edge.city2) or (from_node == edge.city2 and to_node == edge.city1):
+                
+                return int(edge.weight)
+            
+        return None
+    
+    def get_cost_to_destiny(self, from_node, to_node):
+        
+        for directPath in self.directDistancePaths:
+            
+            if (from_node == directPath.city1 and to_node == directPath.city2) or (from_node == directPath.city2 and to_node == directPath.city1):
+                
+                return int(directPath.weight)
+            
+        return None
 
-        count = 0
-        while count < len(self.route)-1:
-            for path in self.countryMap.pathsMap:
-                if path.city1 == self.route[count] and path.city2 == self.route[count+1] or path.city2 == self.route[count] and path.city1 == self.route[count+1]:
-                    self.selectedPath.append(path)
-            count = count + 1
+    def formatIterationList(self):
+        
+        newIterationItem = []
+        newIterationList = []
+        for item in self.iterationList:
+            for i in item:
+                newIterationItem.append(str(i[2]) + "[" + str(i[1]) + "+" +  str(i[0] - i[1]) + "]=" + str(i[0]))
+            newIterationList.append(str(newIterationItem.copy()))
+            newIterationItem.clear()
+         
+        self.iterationList = newIterationList.copy()
+        print (self.iterationList)
+        
+    def astar(self, origin, destiny, isLimited):
+        
+        visited = set()
+        queue = []
+        queue.append((self.get_cost_to_destiny(origin, destiny),0, origin))
+        
+        if isLimited == True:
+            while len(queue) > 0:
+                self.iterationList.append(queue.copy())
+                cost_to_destiny, cost, node = min(queue)
+                queue.remove(min(queue))
+                if node not in visited:
+                    visited.add(node)
+                    if node == destiny:
+                        return
+                    for i in self.neighbors(node):
+                        if i not in visited:
+                            total_cost = cost + self.get_cost(node, i)
+                            cost_to_destiny = total_cost + self.get_cost_to_destiny(i, destiny)
+                            queue.append((cost_to_destiny, total_cost, i))
+                            self.pathList.append((total_cost, i, node))
+        else:
+            while len(queue) > 0:
+                if len(self.pathList) == sys.maxsize:
+    
+                    self.iterationList.clear()
+                    self.iterationList.append("Loop encontrado")
+                    self.pathList.clear()
+                    return  
+                self.iterationList.append(queue.copy())
+                cost_to_destiny, cost, node = min(queue)
+                queue.remove(min(queue))
+                if node == destiny:
+                    return
+                for i in self.neighbors(node):
+                    total_cost = cost + self.get_cost(node, i)
+                    cost_to_destiny = total_cost + self.get_cost_to_destiny(node, destiny)
+                    queue.append((cost_to_destiny, total_cost, i))
+                    self.pathList.append((total_cost, i, node))
+                    
+              
 
-    def searchAstar(self, paths, origin, destiny, currentNode, route):
-        if currentNode == destiny:
-            print("Arrived - " + destiny)
-            return True
-
-        for path in paths:
-            if path.city1 == currentNode:
-                for directPath in self.directDistancePaths:
-                    if directPath.city1 == path.city2 and directPath.city2 == destiny:
-                        self.minDirectDistanceToDestiny[directPath.city1] = directPath.weight
-                    elif directPath.city2 == path.city2 and directPath.city1 == destiny:
-                        self.minDirectDistanceToDestiny[directPath.city2] = directPath.weight
-            elif path.city2 == currentNode:
-                for directPath in self.directDistancePaths:
-                    if directPath.city1 == path.city1 and directPath.city2 == destiny:
-                        self.minDirectDistanceToDestiny[directPath.city1] = directPath.weight
-                    elif directPath.city2 == path.city1 and directPath.city1 == destiny:
-                        self.minDirectDistanceToDestiny[directPath.city2] = directPath.weight
-
-        if len(self.minDirectDistanceToDestiny) > 0:
-            self.minDirectDistanceToDestiny.pop(currentNode)
-            nextNode = self.getMinDirectDistanceToDestiny(
-                self.minDirectDistanceToDestiny)
-            print(self.minDirectDistanceToDestiny)
-            # print(nextNode)
-            # print(self.minDirectDistanceToDestiny)
-            if self.searchAstar(self.graph[nextNode], origin, destiny, nextNode, route) == True:
-                route.append(nextNode)
-                return True
-
-        return False
-
-    # function to return the second element of the
-    # two elements passed as the parameter
-    def sortSecond(self, val):
-        return val[1]
-
-    def getMinDirectDistanceToDestiny(self, minDirectDistanceToDestiny):
-
-        minWeight = 100000
-        nextCity = ""
-
-        nextHopCandidateList = []
-
-        for city, weight in minDirectDistanceToDestiny.items():
-
-            nextHopCandidateList.append([city, weight])
-
-            if int(weight) < minWeight:
-                minWeight = int(weight)
-                nextCity = city
-
-        # sorts the array in ascending according to
-        # second element
-        nextHopCandidateList.sort(key=self.sortSecond)
-        self.iterationList.append(str(nextHopCandidateList))
-        print(nextHopCandidateList)
-
-        return nextCity
